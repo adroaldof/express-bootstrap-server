6b93077d46eec334c3325716ca919ffe4e33c017
Adroaldo de Andrade <adroaldof@gmail.com>
2018-02-08 16:49:02 -0200

    Add initial config file

---
+++ config/env/index.js
@@ -0,0 +1,18 @@
+import path from 'path';
+
+
+const env = process.env.NODE_ENV || 'development';
+const root = path.join(__dirname, '/../..');
+
+const app = {
+  host: process.env.APP_HOST || 'localhost',
+  name: 'Generic Server App',
+  port: process.env.APP_PORT || 3000,
+};
+
+
+export default {
+  app,
+  env,
+  root,
+};
