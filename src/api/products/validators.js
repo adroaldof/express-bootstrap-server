5f5a0f491d5c76562eaa973e9c8bbafe70d9247b
Adroaldo de Andrade <adroaldof@gmail.com>
2018-02-12 19:49:27 -0200

    Add create product validator

--- src/api/products/validators.js
+++ src/api/products/validators.js
@@ -1,10 +1,31 @@
 import joi from 'joi';
 
+const stripUnknown = { stripUnknown: true };
+
+function validate (data, schema, options = {}) {
+  return joi.validate(data, schema, options);
+}
 
 export function validateId (data) {
   const schema = {
     id: joi.number().integer().min(0),
   };
 
-  return joi.validate(data, schema, { stripUnknown: true });
+  return validate(data, schema, stripUnknown);
+}
+
+
+export function validateProduct (data) {
+  const schema = {
+    barCode: joi.string(),
+    description: joi.string(),
+    image: joi.string(),
+    model: joi.string(),
+    name: joi.string().required(),
+    price: joi.number().required(),
+    tradeMark: joi.string(),
+    type: joi.string().valid(['drink', 'food', 'cloths', 'accessories']),
+  };
+
+  return validate(data, schema, stripUnknown);
 }
