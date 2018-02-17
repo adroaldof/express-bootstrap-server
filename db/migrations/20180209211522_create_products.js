e3c26dac4d81755356bc3cacaac36593ab0efea9
Adroaldo de Andrade <adroaldof@gmail.com>
2018-02-17 12:21:01 -0200

    Update migration

--- db/migrations/20180209211522_create_products.js
+++ db/migrations/20180209211522_create_products.js
@@ -1,25 +1,49 @@
 const tableName = 'products';
 
+async function createTable (knex, tableName) {
+  try {
+    await knex
+      .schema
+      .createTable(tableName, function (table) {
+        table.increments('id').primary().unique();
+        table.text('name');
+        table.text('type');
+        table.text('description');
+        table.text('tradeMark');
+        table.text('model');
+        table.text('image');
+        table.text('barCode').unique();
+        table.decimal('price');
+        table.timestamp('createdAt').defaultTo(knex.fn.now());
+        table.timestamp('updatedAt').defaultTo(knex.fn.now());
+      });
+  } catch (error) {
+    console.error(`Error creating table (${tableName}) migration`, error);
+  }
+}
+
 exports.up = async function (knex, Promise) { // eslint-disable-line no-unused-vars
-  await knex
-    .schema
-    .createTableIfNotExists(tableName, function (table) {
-      table.increments('id').primary().unique();
-      table.text('name');
-      table.text('type');
-      table.text('description');
-      table.text('tradeMark');
-      table.text('model');
-      table.text('image');
-      table.text('barCode').unique();
-      table.decimal('price');
-      table.timestamp('createdAt').defaultTo(knex.fn.now());
-      table.timestamp('updatedAt').defaultTo(knex.fn.now());
-    });
+  try {
+    const exists = await knex.schema.hasTable(tableName);
+
+    if (!exists) {
+      await createTable(knex, tableName);
+    }
+  } catch (error) {
+    console.error(`Create table (${tableName}) exists`, error);
+  }
 };
 
 exports.down = async function (knex, Promise) { // eslint-disable-line no-unused-vars
-  await knex
-    .schema
-    .dropTableIfExists(tableName);
+  try {
+    const exists = await knex.schema.hasTable(tableName);
+
+    if (!exists) {
+      await knex
+        .schema
+        .dropTableIfExists(tableName);
+    }
+  } catch (error) {
+    console.error(`Create table (${tableName}) exists`, error);
+  }
 };
