eae9b8759f5a78bc94e0256d881d4847d23a5925
Adroaldo de Andrade <adroaldof@gmail.com>
2018-02-17 08:58:27 -0200

    Update config envs

--- config/index.js
+++ config/index.js
@@ -7,9 +7,9 @@ const env = process.env.NODE_ENV || 'development';
 const root = path.join(__dirname, '/../..');
 
 const app = {
-  host: process.env.APP_HOST || 'localhost',
+  host: process.env.HOST || 'localhost',
   name: 'Generic Server App',
-  port: process.env.APP_PORT || 3000,
+  port: process.env.PORT || 3000,
 };
 
 const queryBuilder = knex(config);
